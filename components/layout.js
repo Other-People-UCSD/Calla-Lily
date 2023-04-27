6d66b6be8aa449e0e729cb2e705fc422d42f853b
Kevin-Jjang <k1jang@ucsd.edu>
2023-04-27 15:35:05 -0700

    init tinacms, removed base path

--- components/layout.js
+++ components/layout.js
@@ -18,14 +18,14 @@ export default function Layout({children, post, genre, title}) {
   return (
     <>
       <Head>
-        <link rel="apple-touch-icon" sizes="180x180" href="/Calla-Lily/favicons/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/Calla-Lily/favicons/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/Calla-Lily/favicons/favicon-16x16.png" />
-        <link rel="manifest" href="/Calla-Lily/favicons/site.webmanifest" crossOrigin="use-credentials"/>
-        <link rel="mask-icon" href="/Calla-Lily/favicons/safari-pinned-tab.svg" color="#5bbad5" />
-        <link rel="shortcut icon" href="/Calla-Lily/favicons/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+        <link rel="manifest" href="/favicons/site.webmanifest" crossOrigin="use-credentials"/>
+        <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#5bbad5" />
+        <link rel="shortcut icon" href="/favicons/favicon.ico" />
         <meta name="msapplication-TileColor" content="#da532c" />
-        <meta name="msapplication-config" content="/Calla-Lily/favicons/browserconfig.xml" />
+        <meta name="msapplication-config" content="/favicons/browserconfig.xml" />
         <meta name="theme-color" content="#ffffff" />
         <title>{siteTitle}</title>
       </Head>
