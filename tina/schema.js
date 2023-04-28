06c22b4a8b579ebe985847a637b32b5493b8ec23
Kevin-Jjang <k1jang@ucsd.edu>
2023-04-27 22:23:11 -0700

    Working tinacms dev

--- tina/schema.js
+++ tina/schema.js
@@ -1,3 +1,10 @@
+const slugify = (values) => {
+  return `${(values.name || values.title || `document-${Date.now()}`)
+    .toLowerCase()
+    .split(' ')
+    .join('-')}`
+}
+
 export const schema = {
   collections: [
     {
@@ -9,6 +16,10 @@ export const schema = {
         router: ({ document }) => {
           `/_posts/${document._sys.filename}`
         },
+        filename: {
+          slugify: slugify,
+          readonly: true
+        },
       },
       defaultItem: () => {
         return {
@@ -19,31 +30,44 @@ export const schema = {
         {
           type: "string",
           name: "title",
-          label: "Title",
+          label: "Title (required)",
           isTitle: true,
           required: true,
         },
         {
           type: "datetime",
           name: "date",
-          label: "Date",
+          label: "Date (required)",
           required: true,
         },
         {
           type: "string",
           name: "contributor",
-          label: "Contributor(s)",
+          label: "Contributor(s) (required)",
           required: true,
         },
         {
           type: "number",
           name: "collection",
-          label: "Collection",
+          label: "Collection No. (leave blank if not part of collection)",
+          ui: {
+            validate: (value) => {
+              return value <= 0 ? "The number must be a positive integer." : null;
+            }
+          }
+        },
+
+        {
+          name: "tags",
+          label: "Tags (Case-sensitive! Please capitalize the first word <3 | required) ",
+          type: "string",
+          list: true,
+          required: true,
         },
         {
           type: "rich-text",
           name: "body",
-          label: "Body",
+          label: "Body (markdown only, NO HTML)",
           isBody: true,
         },
       ],
