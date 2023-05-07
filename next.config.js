f089ef6004140359161b7ca8f3ab4218c582d270
Kevin-Jjang <k1jang@ucsd.edu>
2023-05-07 16:46:17 -0700

    Allow Next Image component to access Tina images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['assets.tina.io'],
+  },
   async redirects() {
     return [
       {
