ebaea0effa77963ddebe9005360e6b5a728b0da1
Kevin-Jjang <k1jang@ucsd.edu>
2023-06-14 17:44:15 -0700

    Restored docs in schema

--- tina/config.js
+++ tina/config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "tinacms";
 import { schema } from "./schema";
 
 // Your hosting provider likely exposes this as an environment variable
-const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "experimental-posts";
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
 
 export default defineConfig({
   branch,
