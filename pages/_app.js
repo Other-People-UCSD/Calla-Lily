bc153709b639a595bbcc7ebc338dd19026769448
Kevin-Jjang <k1jang@ucsd.edu>
2023-07-09 16:13:57 -0700

    Update deps and add Vercel Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -2,18 +2,20 @@ import '@/styles/globals.scss'
 import '@/styles/customPosts.scss';
 import { DefaultSeo } from 'next-seo';
 import { AppWrapper } from '@/components/appContext';
+import { Analytics as VercelAnalytics } from '@vercel/analytics/react';
 import Script from 'next/script';
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Script src="https://www.googletagmanager.com/gtag/js?id=G-CR21DPVQJ3" />
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-CR21DPVQJ3" strategy="afterInteractive" />
       <Script id="google-analytics">
         {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', 'G-CR21DPVQJ3');
+            console.log('analytics!');
           `}
       </Script>
       <DefaultSeo
@@ -27,6 +29,7 @@ export default function App({ Component, pageProps }) {
       <AppWrapper>
         <Component {...pageProps} />
       </AppWrapper>
+      <VercelAnalytics />
     </>
   );
 }
