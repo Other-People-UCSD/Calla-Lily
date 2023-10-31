979afa385096806dce3674089c9d6256ee4fc142
Kevin-Jjang <k1jang@ucsd.edu>
2023-10-30 17:16:08 -0700

    Sync token variables with css

---
+++ stories/TypographyFinal.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from '@emotion/styled';
+
+import styles from './stylesheet.module.scss';
+
+export function TypoVariant({ typography: {size, usage, example, weight, color }, family}) {
+  const exampleStyle = {
+    fontSize: size,
+    fontWeight: weight,
+    color: color,
+    fontFamily: family,
+    margin: 0,
+    lineHeight: 1.2,
+  }
+
+  return (
+    <>
+      <div className={styles["typo-token__item"]}>
+        <div>
+          <p className={styles["typo-token__item__header"]}>Font Size</p>
+          <p className={styles["typo-token__item__info"]}>{size}</p>
+          <p className={styles["typo-token__item__header"]}>Usage</p>
+          <p className={styles["typo-token__item__info"]}>{usage}</p>
+        </div>
+        <div>
+          <p className={styles["typo-token__item__header"]}>Example</p>
+          <p style={exampleStyle}>{example}</p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+
+const FontSize = styled.span(props => ({
+  color: props.color,
+  fontFamily: props.family,
+  marginRight: '8px',
+}));
+
+export default function TypoFamily({ familyName, family, typoFamilyItems }) {
+  return (
+    <div className={styles.family}>
+      <p className={styles["family__header"]}>Font Family</p>
+      <h2>{familyName}</h2>
+      {
+        typoFamilyItems.map((item) => {
+          console.log(item)
+          console.log(family)
+          return <TypoVariant typography={item} family={family} />
+        })
+      }
+    </div>
+  );
+}
+
+TypoFamily.PropTypes = {
+  family: PropTypes.string,
+  typoFamilyItems: PropTypes.array,
+}
+
+TypoVariant.PropTypes = {
+  item: PropTypes.shape({
+    size: PropTypes.string,
+    weight: PropTypes.string,
+    text: PropTypes.string,
+  }),
+}
---
+++ stories/TypographyFinal.stories.js
@@ -0,0 +1,45 @@
+import TypoFamily from './TypographyFinal';
+import * as tokens from './_variables.js';
+
+export default {
+  component: TypoFamily,
+  decorators: [(story) => <div style={{ padding: '3rem' }}>{story()}</div>],
+  title: 'TypographyStyleguide',
+  tags: ['autodocs'],
+}
+
+export const Arial = {
+  args: {
+    familyName: 'Arial',
+    family: tokens.FontArial,
+    typoFamilyItems: [
+      { size: '16px', usage: 'Body', example: 'Lorem ipsum dolor sit amet' },
+      { size: '4rem', usage: 'Titles', example: 'SPRING 2023' },
+      { size: '2rem', usage: 'Landing Page h1s', example: 'Lorem ipsum dolor sit amet' },
+      { size: '1.4rem', usage: 'Submission Page subheadings', example: 'Lorem ipsum dolor sit amet' },
+      { size: '1.2rem', usage: 'Post titles, Genre nav', example: 'Lorem ipsum dolor sit amet' },
+      { size: '1.1rem', usage: 'Long post titles', example: 'Lorem ipsum dolor sit amet' },
+    ],
+  }
+};
+
+export const Basier = {
+  args: {
+    familyName: 'Basier Circle Mono',
+    family: tokens.FontBasier,
+    typoFamilyItems: [
+      { size: '0.8rem', usage: 'Footnotes', example: '[1] [2] [3]' },
+      { size: '0.8rem', usage: 'Subtext (Genre, Copyright)', example: 'Lorem ipsum dolor sit amet' },
+    ],
+  }
+};
+
+export const Apple = {
+  args: {
+    familyName: 'Apple System',
+    family: tokens.FontApple,
+    typoFamilyItems: [
+      { size: '16px', usage: 'Post content', example: 'Lorem ipsum dolor sit amet' },
+    ],
+  }
+}
