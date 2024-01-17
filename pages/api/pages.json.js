fc9155673a3e76bc3088f0668099ca7f83c81653
Kevin-Jjang <k1jang@ucsd.edu>
2024-01-17 14:43:28 -0800

    Output all data in pages function

--- pages/api/pages.json.js
+++ pages/api/pages.json.js
@@ -3,16 +3,18 @@ import { getLandingPages } from "@/lib/posts";
 export default async function handler(req, res) {
   try {
     const landingPages = await getLandingPages();
-    const keyedPages = {};
+    // const keyedPages = {};
 
-    landingPages.map((page) => {
-      const key = `/${page.slug}`;
-      keyedPages[key] = page;
-    });
+    // landingPages.map((page) => {
+    //   const key = `/${page.slug}`;
+    //   keyedPages[key] = page;
+    // });
+    console.log(landingPages)
     res.setHeader('Content-Type', 'application/json');
 
-    return res.status(200).json(keyedPages);
-  } catch {
+    return res.status(200).json(landingPages);
+  } catch (e){
+    console.log(e)
     res.status(500).send({ error: 'Failed to fetch data' });
   }
 }
