a2ca47624e516d69b7194be5ff97351c05ee25f0
Kevin-Jjang <k1jang@ucsd.edu>
2024-04-14 18:59:06 -0700

    Remove unused breaking code from metadata api

--- pages/api/post-metadata.json.js
+++ pages/api/post-metadata.json.js
@@ -2,33 +2,17 @@ import { getSortedPostsAPIData } from "@/lib/posts";
 
 export default async function handler(req, res) {
   try {
-    const sortedPosts = await getSortedPostsAPIData(req.headers['opm-content']);
+    const sortedPosts = await getSortedPostsAPIData();
     const keyedPosts = {};
-    const years = new Set();
-    const collections = new Set();
 
     sortedPosts.map((post) => {
       const key = `/${post.slug}`;
       keyedPosts[key] = post;
-
-      if (post.date) {
-        const date = new Date(post.date);
-        years.add(date.getFullYear());
-      }
-      if (post.collection) {
-        collections.add(post.collection);
-      }
     });
     
-    const metadata = {
-      years: Array.from(years), 
-      collections: Array.from(collections), 
-      ...keyedPosts};
-    
     res.setHeader('Content-Type', 'application/json');
-    return res.status(200).json(metadata);
+    return res.status(200).json(keyedPosts);
   } catch (e){
-    console.log(e)
     res.status(500).send({ error: 'Failed to fetch data' });
   }
 }
